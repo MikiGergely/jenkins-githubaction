00d66de45e9f9d2a3aca2ae8f689d8686c0acdca
MikiGergely <miklos.gergely@intland.com>
2022-06-13 16:49:13 +0200

    fst test

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM alpine
 RUN apk add python3
 
 ADD utils/ /root/utils
-ADD ./main.py /root/
+ADD app/ /root/app/
 ENV PYTHONPATH /root/
 
 ENTRYPOINT [ "/root/main.py" ]
--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
-BUILD_DIR="$1"
-cp -R utils ${BUILD_DIR}
-cp Dockerfile ${BUILD_DIR}
-docker build ${BUILD_DIR} -t "git-runner-container:${BUILD_DIR}"
+rm -rf app/
+cp  -R $1 app
